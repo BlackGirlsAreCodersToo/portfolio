88a1ca98bf719155678a6d46731ae1f858e93e94
HeyItsTay <52268711+BlackGirlsAreCodersToo@users.noreply.github.com>
2022-04-21 20:07:11 -0700

    second command

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hey my name is</div>;
+  return (
+    <div className="App">
+      Hey my name is <div>hellooo</div>
+      <div>Hi guys my name what my name what slim shady </div>
+    </div>
+  );
 }
 
 export default App;
